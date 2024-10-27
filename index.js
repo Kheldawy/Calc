bb8164d23094379a0653e06272a022f402321568
Khaled Ibrahim <khaled.abdo11@gmail.com>
2024-10-27 16:46:28 +0100

    Simple Calc
    Simple Calculator!

---
+++ index.js
@@ -0,0 +1,33 @@
+
+alert("Welcome to the Simple Calculator!");
+
+let firstNumber = parseFloat(prompt("Please enter the first number:"));
+
+let secondNumber = parseFloat(prompt("Please enter the second number:"));
+
+let operation = prompt("Which calculation method would you like to use? (+, -, *, /)").trim();
+
+let result;
+
+if (operation === "+") {
+    result = firstNumber + secondNumber;
+} else if (operation === "-") {
+    result = firstNumber - secondNumber;
+} else if (operation === "*") {
+    result = firstNumber * secondNumber;
+} else if (operation === "/") {
+    if (secondNumber !== 0) {
+        result = firstNumber / secondNumber;
+    } else {
+        alert("Division by zero is not allowed!");
+    }
+} else {
+    alert("Invalid operation! Please use +, -, *, or /.");
+}
+
+
+if (result !== undefined) {
+    alert("The result of " + firstNumber + " " + operation + " " + secondNumber + " is: " + result);
+}
+
+alert("Thank you for using the calculator! Goodbye!");
